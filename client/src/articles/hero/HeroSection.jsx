684f735c73bdbe94861669ec781bb11f837b03a9
Adeleke Adeyemi <jephthahadeleke2003@gmail.com>
2026-05-13 02:27:38 +0100

    glows added

--- client/src/articles/hero/HeroSection.jsx
+++ client/src/articles/hero/HeroSection.jsx
@@ -4,29 +4,56 @@ import HeroImage from './HeroImage'
 
 const HeroSection = () => {
   return (
-    <div className='lg:mt-[75px] px-5 lg:flex justify-between relative'>
+    <div className='lg:mt-[75px] lg:pb-9 px-5 lg:flex justify-between relative'>
 
-      {/* RESPONSIVE GLOWS - Centered container approach */}
-      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-        <div className="relative w-full max-w-7xl h-full">
-          
-          {/* Cyan/Blue Glow */}
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-            w-[80%] h-[70%] 
-            lg:w-[50%] lg:h-[80%]
-            bg-[radial-gradient(ellipse_at_center,_rgba(0,211,243,0.1)_0%,_rgba(0,211,243,0.02)_60%,_transparent_100%)]
-            blur-2xl">
-          </div>
-          
-          {/* Purple/Magenta Glow */}
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-            w-[80%] h-[70%] 
-            lg:w-[50%] lg:h-[80%]
-            bg-[radial-gradient(ellipse_at_center,_rgba(173,70,255,0.1)_0%,_rgba(173,70,255,0.02)_60%,_transparent_100%)]
-            blur-2xl">
-          </div>
-          
-        </div>
+      {/* GLOWS - Exact design specs with responsive adjustments */}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        
+        {/* Purple Glow (#AD46FF) */}
+        <div className="absolute 
+          w-[401.89px] h-[401.89px]
+          md:w-[300px] md:h-[300px]
+          lg:w-[401.89px] lg:h-[401.89px]
+          rounded-full
+          bg-[radial-gradient(circle,_#AD46FF33_0%,_rgba(173,70,255,0.3)_40%,_transparent_80%)]
+          opacity-[52px] blur-[251.18px]"
+          style={{
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+        />
+        
+        {/* Cyan Glow (#00B8DB) */}
+        <div className="absolute 
+          w-[401.89px] h-[401.89px]
+          md:w-[300px] md:h-[300px]
+          lg:w-[401.89px] lg:h-[401.89px]
+          rounded-full
+          bg-[radial-gradient(circle,_#00B8DB33_0%,_rgba(0,184,219,0.3)_40%,_transparent_80%)]
+          opacity-[52px] blur-[251.18px]"
+          style={{
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+        />
+        
+        {/* Combined glow for blending effect */}
+        <div className="absolute 
+          w-[401.89px] h-[401.89px]
+          md:w-[300px] md:h-[300px]
+          lg:w-[401.89px] lg:h-[401.89px]
+          rounded-full
+          bg-[radial-gradient(circle,_rgba(0,184,219,0.15)_0%,_rgba(173,70,255,0.15)_50%,_transparent_100%)]
+          opacity-[52px] blur-[200px]"
+          style={{
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+        />
+        
       </div>
       
       <HeroText />
